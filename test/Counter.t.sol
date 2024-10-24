a64cadf1c4fe7611e7dac2ccc6491d476beccc99
Shreyash Naik <77315155+shrxyeh@users.noreply.github.com>
2024-10-24 19:49:44 +0530

    Update Counter.t.sol

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,30 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {FoundryZkSyncChecker} from "lib/foundry-devops/src/FoundryZkSyncChecker.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract ZkSyncDevOps is Test, ZkSyncChainChecker, FoundryZkSyncChecker {
+    // Remove the `skipZkSync`, then run `forge test --mt testZkSyncChainFails --zksync` and this will fail!
+    function testZkSyncChainFails() public skipZkSync {
+        address ripemd = address(uint160(3));
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        bool success;
+        // Don't worry about what this "assembly" thing is for now
+        assembly {
+            success := call(gas(), ripemd, 0, 0, 0, 0, 0)
+        }
+        assert(success);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+    // You'll need `ffi=true` in your foundry.toml to run this test
+    // // Remove the `onlyVanillaFoundry`, then run `foundryup-zksync` and then
+    // // `forge test --mt testZkSyncFoundryFails --zksync`
+    // // and this will fail!
+    // function testZkSyncFoundryFails() public onlyVanillaFoundry {
+    //     bool exists = vm.keyExistsJson('{"hi": "true"}', ".hi");
+    //     assert(exists);
+    // }
 }
